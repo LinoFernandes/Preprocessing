cf7da94427ff5de995b5b99e60fddabc3bd02f40
LinoFernandes <linoafernandes@tecnico.ulisboa.pt>
2017-05-05 01:01:38 +0100

    update

--- NVparamtuning.py
+++ NVparamtuning.py
@@ -1,27 +1,39 @@
 import numpy as np
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
 import sys
+from sklearn.feature_selection import RFE
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import os
+import datetime as dt
+
 Window = np.array([90, 180, 365])
-
-def categoricalData(data):
-    from scipy.stats import mode
-    Counts = mode(data)
-    return Counts.mode[0]
-
 for window in Window:
-    for seed in range(1,2):
-        path = '/Users/Lino/PycharmProjects/Preprocessing/FOLDS/' + str(window) + 'd_FOLDS/S' + str(seed)
+    for seed in range(1,6):
+        if sys.platform == "darwin":
+            path = '/Users/Lino/PycharmProjects/Preprocessing/PreProcessedFoldsFinal/' + str(window) + 'd_FOLDS/S' + str(
+                seed)
+        elif sys.platform == "win32":
+            path = 'C:\\Users\\Lino\\PycharmProjects\\Preprocessing\\PreProcessedFoldsFinal\\' + str(
+                window) + 'd_FOLDS\\S' + str(seed)
         sys.path.append(path)
-        for fold in range(1,2):
-            #print(path + '/' + str(window) + 'd_FOLDS_train_' + str(fold))
-            Train = pd.read_csv(path + '/' + str(window) + 'd_FOLDS_train_' + str(fold) + '.csv')
-            Test = pd.read_csv(path + '/' + str(window) + 'd_FOLDS_test_' + str(fold) + '.csv')
-            X = Train.iloc[:,:-2].values
-            Y = Train.iloc[:,np.shape(Train)[1]-2].values
-            #print(np.shape(X))
-            print(X)
-            #model = GaussianNB()
-            #pred = model.partial_fit()
+        for fold in range(1,11):
+            try:
+                if sys.platform == "darwin":
+                    TrainPD = pd.read_csv(path + '/' + str(window) + 'd_FOLDS_train_' + str(fold) + '.csv')
+                    TestPD = pd.read_csv(path + '/' + str(window) + 'd_FOLDS_test_' + str(fold) + '.csv')
+                    directory = ('/Users/Lino/PycharmProjects/Preprocessing/NV/' + str(window) + 'd_FOLDS/S' + str(seed) + '/')
+                elif sys.platform == "win32":
+                    TrainPD = pd.read_csv(path + '\\' + str(window) + 'd_FOLDS_train_' + str(fold) + '.csv')
+                    TestPD = pd.read_csv(path + '\\' + str(window) + 'd_FOLDS_test_' + str(fold) + '.csv')
+                    directory = ('C:\\Users\\Lino\\PycharmProjects\\Preprocessing\\NV\\' + str(window) + 'd_FOLDS\\S' + str(seed) + '\\')
+            except FileNotFoundError:
+                continue
 
-
+            # Create the RFE object and rank each pixel
+            NB = GaussianNB()
+            svc = SVC(C = 1, kernel = 'linear')
+            rfe = RFE(estimator=svc)
+            rfe.fit(TrainPD.ix[:, TrainPD.columns != 'Evolution'], TrainPD.ix[:, 'Evolution'])
+            ranking = rfe.ranking_()
+            print(ranking)
--- Preprocessing.py
+++ Preprocessing.py
@@ -2,24 +2,37 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+import datetime as dt
 
 Window = np.array([90, 180, 365])
 for window in Window:
     for seed in range(1,6):
-        path = 'C:\\Users\\Lino\\PycharmProjects\\Preprocessing\\PreProcessedFolds\\' + str(window) + 'd_FOLDS\\S' + str(seed)
+        if sys.platform == 'win32':
+            path = 'C:\\Users\\Lino\\PycharmProjects\\Preprocessing\\PreProcessedFolds\\' + str(window) + 'd_FOLDS\\S' + str(seed)
+        elif sys.platform == 'darwin':
+            path = '/Users/Lino/PycharmProjects/Preprocessing/PreProcessedFolds/' + str(window) + 'd_FOLDS/S' + str(seed)
         sys.path.append(path)
         for fold in range(1,11):
-            TrainPD = pd.read_csv(path + '\\' + str(window) + 'd_FOLDS_train_' + str(fold) + '.csv')
-            TestPD = pd.read_csv(path + '\\' + str(window) + 'd_FOLDS_test_' + str(fold) + '.csv')
-            Train = TrainPD.values
-            Test = TestPD.values
-
+            try:
+                if sys.platform == 'win32':
+                    TrainPD = pd.read_csv(path + '\\' + str(window) + 'd_FOLDS_train_' + str(fold) + '.csv')
+                    TestPD = pd.read_csv(path + '\\' + str(window) + 'd_FOLDS_test_' + str(fold) + '.csv')
+                elif sys.platform == 'darwin':
+                    TrainPD = pd.read_csv(path + '/' + str(window) + 'd_FOLDS_train_' + str(fold) + '.csv')
+                    TestPD = pd.read_csv(path + '/' + str(window) + 'd_FOLDS_test_' + str(fold) + '.csv')
+            except FileNotFoundError:
+                continue
             auxTrain = TrainPD.columns[pd.isnull(TrainPD).any()].tolist() #Columns with nans
             auxTest = TestPD.columns[pd.isnull(TestPD).any()].tolist() #Columns with nans
             from sklearn.preprocessing import Imputer
-            imputer = Imputer(missing_values='NaN', strategy='mean',axis=0);
+            imputer = Imputer(missing_values='NaN', strategy='mean',axis=0)
             TrainPD[auxTrain] = imputer.fit_transform(TrainPD[auxTrain])
             TestPD[auxTest] = imputer.fit_transform(TestPD[auxTest])
+
+            # from sklearn.preprocessing import StandardScaler
+            # SC = StandardScaler()
+            # TrainPD[auxTrain] = SC.fit_transform(TrainPD[auxTrain].values)
+            # TestPD[auxTest] = SC.fit_transform(TestPD[auxTest].values)
 
             from sklearn.preprocessing import LabelEncoder
             LE = LabelEncoder()
@@ -27,18 +40,48 @@ for window in Window:
             TestPD['Evolution'] = LE.fit_transform(TestPD['Evolution'])
 
             CatCols = [5, 7, 8]
+            CatCols=TrainPD[TrainPD.columns.values[CatCols]]
             for col in CatCols:
-                dummies = pd.get_dummies(TrainPD[TrainPD.columns.values[col]])
+                if col == 'El Escorial reviewed criteria':
+                    Words = ['PMA', 'def', 'pos', 'pro', 'sus']
+                elif col == 'Envolved segment - 1st symptoms':
+                    Words = ['B', 'B,AR', 'LL', 'UL']
+                else:
+                    Words = ['hemi', 'hemi cruz', 'hemi para', 'para', 'para cruz']
+                dummiesTrain = pd.get_dummies(TrainPD[col], columns=Words, drop_first=True)
+                dummiesTest = pd.get_dummies(TestPD[col], columns=Words, drop_first=True)
+                i = 0
+                auxTrain = np.zeros(shape=[len(TrainPD), len(Words)])
+                auxTest = np.zeros(shape=[len(TestPD), len(Words)])
+                for word in Words:
+                    CheckTrain = TrainPD[col] == word
+                    CheckTest = TestPD[col] == word
+                    auxTrain[CheckTrain, i] = 1
+                    auxTest[CheckTest,i] = 1
+                    i += 1
+                ColLoc = TrainPD.columns.get_loc(col)
+                TrainPD = TrainPD.drop(col, 1)
+                TestPD = TestPD.drop(col,1)
+                i = 0
+                for dummy in range(0,len(Words)):
+                    TrainPD.insert(ColLoc + i, Words[dummy], auxTrain[:,dummy])
+                    TestPD.insert(ColLoc + i, Words[dummy], auxTest[:, dummy])
+                    i += 1
 
-
-            NewTrain = pd.DataFrame(Train, index = None, columns = TrainPD.columns.values)
-            NewTest = pd.DataFrame(Test, index = None, columns = TestPD.columns.values)
-            directory = ('C:\\Users\\Lino\\PycharmProjects\\Preprocessing\\PreProcessedFolds\\' + str(window) + 'd_FOLDS\\S' + str(seed) + '\\')
+            from sklearn.preprocessing import MinMaxScaler
+            SC = MinMaxScaler()
+            SCCol=[x for x in TrainPD.columns.values if x not in ['Name','Evolution']]
+            TrainPD[SCCol] = SC.fit_transform(TrainPD.ix[:, SCCol])
+            TestPD[SCCol] = SC.fit_transform(TestPD.ix[:, SCCol])
+            if sys.platform == 'win32':
+                directory = ('C:\\Users\\Lino\\PycharmProjects\\Preprocessing\\PreProcessedFoldsFinal\\' + str(window) + 'd_FOLDS\\S' + str(seed) + '\\')
+            elif sys.platform == 'darwin':
+                directory = ('/Users/Lino/PycharmProjects/Preprocessing/PreProcessedFoldsFinal/' + str(window) + 'd_FOLDS/S' + str(seed) + '/')
             if not os.path.exists(directory):
                 os.makedirs(directory)
             filenameTrain = str(window) + 'd_FOLDS_train_' + str(fold) + '.csv'
             filenameTest = str(window) + 'd_FOLDS_test_' + str(fold) + '.csv'
             os.path.join(directory, filenameTrain)
-            NewTrain.to_csv(directory+filenameTrain, sep=',')
+            TrainPD.to_csv(directory+filenameTrain, sep=',',index=False)
             os.path.join(directory, filenameTest)
-            NewTest.to_csv(directory+filenameTest, sep=',')
+            TestPD.to_csv(directory+filenameTest, sep=',',index=False)
