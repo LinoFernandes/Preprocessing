2e0f5fc01d8e0abf04c6680266cb17afd2ff9a5c
LinoFernandes <linoafernandes@tecnico.ulisboa.pt>
2017-05-15 02:27:12 +0100

    update

--- TimeEvoPop.py
+++ TimeEvoPop.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
+
+
 if sys.platform == "darwin":
     ALSFRS = pd.read_csv('/Users/Lino/PycharmProjects/Preprocessing/alsfrs.csv', na_values=np.NaN)
     Demographics = pd.read_csv('/Users/Lino/PycharmProjects/Preprocessing/demo.csv', na_values=np.NaN)
@@ -26,5 +28,22 @@ for entry in range(0,len(ALSFRS)):
         Score.append(score)
     except ValueError:
         Score.append(np.NaN)
+Score = np.array(Score)
+NotNaN = [i for i in range(0, len(Score)) if not np.isnan(Score[i]) or Score[i] > 0]
 
+hist, bins = np.histogram(Score[NotNaN], bins=1000)
+freq = 5
+hist[np.where(hist <= freq)] = 0
+z=np.percentile(Score[NotNaN], np.arange(0, 100, 25))
 
+# Plot
+bins = bins[hist != 0]
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+print(len(bins))
+print(len(hist[hist!=0]))
+print(center)
+plt.bar(bins, hist[hist!=0], align='center', width=width)
+for line in z:
+    plt.axvline(x=line)
+plt.savefig('TimeEvo.png')
